fc90b632024d73ad3c514775040489c277819771
Andrew <tdinerd12@gmail.com>
2025-09-02 14:23:15 -0400

    draw player to screen.  base player on circleshape for future collision detection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame import Color
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -10,11 +11,16 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(Color(0,0,0))
+        player.draw(screen)
+
+        # End of Game Loop
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
